f374ca529ca3fbdcfdca2d62e128aaaeb7bc9576
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2022-12-15 20:54:06 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,14 @@
+class Product 
+{
+	public static void main(String[] args) 
+	{
+		int n  = 1;
+		int mul = 1;
+		while (n<=5)
+		{
+			mul = mul*n;
+			n++;
+		}
+		System.out.println(mul);
+	}
+}
