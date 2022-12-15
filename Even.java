6d11d6e5ea71a6aaaa1e1aaec0d0233fbe80f8a2
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2022-12-15 20:17:46 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+class Even 
+{
+	public static void main(String[] args) 
+	{
+		int n = 1;
+		while (n<=5)
+		{
+			if (n%2==0)
+			{
+         System.out.println(n);
+			}
+			n++;
+		}
+		
+	}
+}
