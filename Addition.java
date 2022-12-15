a318ed4884699bd7af350ee3a07182423f56f251
shivani yadav <112896644+shivaniyuvrajyadav@users.noreply.github.com>
2022-12-15 21:02:09 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Addition 
+{
+	public static void main(String[] args) 
+	{
+		int n = 1;
+		int sum = 0;
+		while (n<=5)
+		{
+			sum = sum + n;
+			n++;
+		}
+		System.out.println("sum:"+sum);
+	}
+}
